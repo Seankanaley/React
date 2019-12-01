e9acf2fb2a0b8fcd04d3fddc2d3eed0bb4fb557a
Sean Kanaley <sean@postscriptlabs.com>
2019-12-01 15:49:25 -0800

    Shared Components, lifting the state

--- nucampsite/src/components/DirectoryComponent.js
+++ nucampsite/src/components/DirectoryComponent.js
@@ -17,43 +17,12 @@ import React, { Component } from 'react';
 class Directory extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            campsites: [
-                {
-                    id: 0,
-                    name: 'React Lake Campground',
-                    image: 'assets/images/react-lake.jpg',
-                    elevation: 1233,
-                    description: "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers."
-                },
-                {
-                    id: 1,
-                    name: 'Chrome River Campground ',
-                    image: 'assets/images/chrome-river.jpg',
-                    elevation: 877,
-                    description: "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River."
-                },
-                {
-                    id: 2,
-                    name: 'Breadcrumb Trail Campground',
-                    image: 'assets/images/breadcrumb-trail.jpg',
-                    elevation: 2901,
-                    description: "Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground."
-                },
-                {
-                    id: 3,
-                    name: 'Redux Woods Campground',
-                    image: 'assets/images/redux-woods.jpg',
-                    elevation: 42,
-                    description: "You'll never want to leave this hidden gem, deep within the lush Redux Woods."
-                }
-            ]
-        };
+        this.state = {};
     }
 
 
     render() {
-        const directory = this.state.campsites.map(campsite => {
+        const directory = this.props.campsites.map(campsite => {
             return (
                 //To render and array of elements most efficiently, add a unique key attribue to the topmost element in each array key="element.id"
                 <div key={campsite.id} className="col">
@@ -73,5 +42,25 @@ class Directory extends Component {
         );
     }
 }
+
+//Example for component usage State/Props
+
+// class ExampleParentComponent extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             number: 333
+//         }
+//     }
+//     render(){
+//         return <ExampleChildComponent number={this.state.number} />
+//     };
+// }
+
+// class ExampleChildComponent extends Component {
+//     render(){
+//         return <div>{this.props.number}</div>
+//     };
+// }
 
 export default Directory;
